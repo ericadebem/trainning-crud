e843ed7445aaa5c7333ec3a62783450985afdde4
ericadebem <110907550+ericadebem@users.noreply.github.com>
2022-10-18 21:57:17 +0200

    first commit

---
+++ assets/main.js
@@ -0,0 +1,57 @@
+'use strickt'
+
+const openMdal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => document.getElementById('modal')
+    .classList.remove('active')
+
+const tempClient = {
+    nome: "Erica",
+    email: "ericadebemm@gmail.com",
+    celular: "0123088990",
+    cidade: "Weimar"
+}  
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('dbClient')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
+
+// CRUD - create read update delete
+
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage(dbClient)
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+}
+
+//interacao layout
+const saveClient = () => {
+    if (isValidFields()) {
+        console.log("cadastrando clientes")
+    }
+}
+
+//eventos
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', closeModal)
+    
+document.getElementById('modalClose')
+    .addEventListener('click', closeModal)    
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
